3ab61d039cb7bd3d9188e4229ff7ca1b9d434888
John Corser <johnpc@umich.edu>
2024-01-08 13:22:37 -0500

    add silas

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import silas from '$lib/images/silas.jpeg';
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
@@ -6,14 +10,33 @@
 <div class="text-column">
 	<h1>About this app</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app with
-		<a href="https://docs.amplify.aws/gen2/start/quickstart/">AWS Amplify Gen2</a>. You can make
-		your own by typing the following into your command line and following the prompts:
+	<p style="text-align: center;">
+		<img width="50%" style="border-radius: 500px;" src={silas} alt="Silas" />
 	</p>
 
-	<pre>npm create svelte@latest</pre>
-	<pre>npm create amplify@latest</pre>
+	<p>Meet Silas, Founder of Small Crosswords</p>
+	<p>
+		Silas is a Cannes Lion awarded marketer turned entrepreneur on a mission to create bite-sized
+		crossword puzzles for busy puzzle lovers.
+	</p>
+	<p>
+		With over 10 years of experience leading growth strategy and digital marketing for major brands
+		like Deloitte, UPS, and Sony, Silas brings his passion for innovation to Small Crosswords.
+	</p>
+	<p>
+		At Small Crosswords, Silas draws on his expertise in e-commerce, SaaS, and online growth to
+		drive the brand while staying true to its vision of simple, entertaining crossword puzzles.
+	</p>
+	<p>
+		In his free time, you can find Silas trying new word games, reading the newspaper, and spending
+		time with family. As an avid cruciverbalist, he always has a crossword handy to challenge his
+		problem-solving skills.
+	</p>
+	<p>
+		Silas is dedicated to making Small Crosswords a beloved brand for on-the-go crossword fans
+		looking for a puzzle break anytime, anywhere. With his marketing savvy and passion for engaging
+		games, he leads the brand in creating fun, bite-sized crosswords perfect for busy solvers.
+	</p>
 
 	<p>
 		If you'd like to learn how this app works (or contribute!) check out the source code on <a
