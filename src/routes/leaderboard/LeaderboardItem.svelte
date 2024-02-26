e73f79ad2868247dc9ec7fbfd59d998399c01f8b
John Corser <johnpc@umich.edu>
2024-02-26 00:02:29 -0500

    highlight logged in user on leaderboard

--- src/routes/leaderboard/LeaderboardItem.svelte
+++ src/routes/leaderboard/LeaderboardItem.svelte
@@ -5,18 +5,32 @@
 	import config from '../../amplifyconfiguration.json';
 	import { getAllUserPuzzles } from '../helpers/getAllUserPuzzles';
 	import type { HydratedUserPuzzle } from '../helpers/types/types';
+	import { getCurrentUser } from 'aws-amplify/auth';
 	Amplify.configure(config);
 	export let profile: Schema['Profile'];
 
+	$: currentUserId = '';
 	$: completedPuzzles = [] as HydratedUserPuzzle[];
 	onMount(() => {
 		const setup = async () => {
 			const completedPuzzlesResponse = await getAllUserPuzzles(profile, true);
 			completedPuzzles = completedPuzzlesResponse;
+			try {
+				const currentUser = await getCurrentUser();
+				currentUserId = currentUser.userId;
+			} catch {}
 		};
 
 		setup();
 	});
 </script>
 
-<li>{profile.name.split('@')[0]} ({completedPuzzles.length})</li>
+<li class={currentUserId === profile.userId ? 'loggedInProfile' : ''}>
+	{profile.name.split('@')[0]} ({completedPuzzles.length})
+</li>
+
+<style>
+	.loggedInProfile {
+		color: palevioletred;
+	}
+</style>
