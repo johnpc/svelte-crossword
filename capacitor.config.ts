87510ab0cc44d1ca1b1099a15ddc8b59ded5d17d
John Corser <johnpc@umich.edu>
2024-03-06 10:36:59 -0500

    setup android release

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,4 +1,6 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const config: CapacitorConfig = {
 	appId: 'com.johncorser.crosswords',
@@ -11,6 +13,14 @@ const config: CapacitorConfig = {
 	ios: {
 		contentInset: 'always',
 		backgroundColor: '#DB7093'
+	},
+	android: {
+		buildOptions: {
+			keystorePath: process.env.ANDROID_KEYSTORE_PATH,
+			keystorePassword: process.env.ANDROID_KEYSTORE_PASSWORD,
+			keystoreAlias: process.env.ANDROID_KEYSTORE_ALIAS,
+			keystoreAliasPassword: process.env.ANDROID_KEYSTORE_ALIAS_PASSWORD
+		}
 	}
 };
 
