17e42e2e642da1f8042f53f96345f373b61d65b0
John Corser <johnpc@umich.edu>
2026-06-19 22:36:04 -0400

    fix(ios): disable status bar tap scroll-to-top (#33)
    Set scrollsToTop = false on the WKWebView's scroll view so tapping
    the iOS status bar no longer jumps the crossword back to the top.

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -7,8 +7,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        if let window = window,
+           let rootViewController = window.rootViewController as? CAPBridgeViewController {
+            rootViewController.webView?.scrollView.scrollsToTop = false
+        }
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -23,10 +29,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
